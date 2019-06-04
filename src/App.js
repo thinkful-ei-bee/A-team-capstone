d4efa5768f67e2e5440fa02d14effed6dbc2aee0
quonn <qber83@gmail.com>
2019-06-03 23:44:48 -0700

    removed unused files and added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import { Route, Switch } from "react-router-dom";
-import { BrowserRouter as Router, Link, Redirect } from 'react-router-dom'
+import TopNav from './Components/TopNav/TopNav'
+import Login from './routes/Login/Login'
 
 function App() {
   return (
     <div className="App">
-      
+      <TopNav></TopNav>
+      <main>
+          <Switch>
+            {/* <Route exact path={"/"} component={Landing} />
+            <Route path={"/bmFeed"} component={BlogFeed} /> */}
+            <Route path={"/login"} component={Login} />
+            {/* <Route path={"/register"} component={Registration} />
+            <PrivateRoute path={"/create_post"} component={Create_Post} />
+            <Route exact path={"/post/:postId"} render={props => <PostPage {...props}/>}/>
+            <Route exact path={"/result/:resultId"} render={props => <ResultPage {...props}/>}/>
+            <Route exact path={"/account/:username"} render={props => <SubscriberPage {...props}/>}/>
+            <Route component={PageNotFound}/> */}
+          </Switch>
+          {/* <Footer></Footer> */}
+        </main>
     </div>
   );
 }
