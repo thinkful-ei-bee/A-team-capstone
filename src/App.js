13a75b3b3f99af263b3cb5fa33005ed946d0b11d
quonn <qber83@gmail.com>
2019-06-03 22:32:47 -0700

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Link, Redirect } from 'react-router-dom'
 
 function App() {
   return (
