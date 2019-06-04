776f8ee9bcb4df55e1dd60b6d034da22fee7e0a9
Eddie Bueno <eddiebueno93@gmail.com>
2019-06-04 14:21:29 -0700

    add profile route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import MainPage from './Components/MainPage/MainPage'
 import Login from './routes/Login/Login'
 import Registration from './routes/Registration/Registration'
 import NewProjectForm from './Components/NewProjectForm/NewProjectForm';
+import Profile from './Components/Profile/Profile';
 
 function App() {
   return (
@@ -16,7 +17,9 @@ function App() {
           <Route path={"/login"} component={Login} />
           <Route path={"/register"} component={Registration} />
           <Route path={"/new"} component={NewProjectForm} />
+          
         </Switch>
+        <Route exact path={"/users/3"} component={Profile} />
       </main>
     </div>
   );
