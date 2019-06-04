d33e5d78b781b25c035f0ca8b8875dd09020248e
quonn <qber83@gmail.com>
2019-06-04 06:14:38 -0700

    Added Registration to App, minus styling

--- src/App.js
+++ src/App.js
@@ -2,25 +2,18 @@ import React from 'react';
 import { Route, Switch } from "react-router-dom";
 import TopNav from './Components/TopNav/TopNav'
 import Login from './routes/Login/Login'
+import Registration from './routes/Registration/Registration'
 
 function App() {
   return (
     <div className="App">
       <TopNav></TopNav>
       <main>
-          <Switch>
-            {/* <Route exact path={"/"} component={Landing} />
-            <Route path={"/bmFeed"} component={BlogFeed} /> */}
-            <Route path={"/login"} component={Login} />
-            {/* <Route path={"/register"} component={Registration} />
-            <PrivateRoute path={"/create_post"} component={Create_Post} />
-            <Route exact path={"/post/:postId"} render={props => <PostPage {...props}/>}/>
-            <Route exact path={"/result/:resultId"} render={props => <ResultPage {...props}/>}/>
-            <Route exact path={"/account/:username"} render={props => <SubscriberPage {...props}/>}/>
-            <Route component={PageNotFound}/> */}
-          </Switch>
-          {/* <Footer></Footer> */}
-        </main>
+        <Switch>
+          <Route path={"/login"} component={Login} />
+          <Route path={"/register"} component={Registration} />
+        </Switch>
+      </main>
     </div>
   );
 }
