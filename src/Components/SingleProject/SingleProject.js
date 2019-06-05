57bffaf38e848f7e84792d29ef87ae42158d3549
quonn <qber83@gmail.com>
2019-06-05 10:09:37 -0700

    added header and article to separate header from project content

--- src/Components/SingleProject/SingleProject.js
+++ src/Components/SingleProject/SingleProject.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import TokenService from '../../services/token-service';
-class SingleProject extends React.Component{
+class SingleProject extends React.Component {
 
     renderBidButton = () => {
         return <>
-        <button className="btn">Bid!</button>
+            <button className="btn">Bid!</button>
         </>
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <article className="main-single-project-square">
-                <h3>Project Title:</h3>
-                <p>Project Description:</p>
-                <p>Languages:</p>
-                <p>Minimum Reqs:</p>
-                <p># Developers Needed:</p>
-                <p>Deadline:</p>
-                <p>Personnel Count:</p>
+                <header>
+                    <h3>Project Title:</h3>
+                </header>
+                <article>
+                    <p>Project Description:</p>
+                    <p>Languages:</p>
+                    <p>Minimum Reqs:</p>
+                    <p># Developers Needed:</p>
+                    <p>Deadline:</p>
+                    <p>Personnel Count:</p>
+                </article>
                 {
                     TokenService.hasAuthToken()
-                    ? this.renderBidButton()
-                    : null
+                        ? this.renderBidButton()
+                        : null
                 }
             </article>
         )
