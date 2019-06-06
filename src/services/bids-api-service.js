787361b11db426854a0dc5e2523024ba32ad662d
Eddie Bueno <eddiebueno93@gmail.com>
2019-06-06 09:24:58 -0700

    add get users bids to bids api service

---
+++ src/services/bids-api-service.js
@@ -0,0 +1,21 @@
+import config from '../config';
+import TokenService from './token-service';
+
+const BidsApiService = {
+  getUsersBids(){
+    return fetch(`${config.API_ENDPOINT}/users/bids`,{
+      method: 'GET',
+      headers:{
+        'Content-Type':'application/json',
+        'Authorization': `bearer ${TokenService.getAuthToken()}`
+      }
+    })
+    .then(res=>
+      (!res.ok) 
+        ? res.json().then(e=>Promise.reject(e))
+        : res.json()
+    );
+  }
+}
+
+export default BidsApiService;
