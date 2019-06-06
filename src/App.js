032d36b507f1bb88cdbad633c8a83ca8bbecad02
Eddie Bueno <eddiebueno93@gmail.com>
2019-06-06 12:48:28 -0700

    merge conflict

--- src/App.js
+++ src/App.js
@@ -6,25 +6,6 @@ import Login from './routes/Login/Login'
 import Registration from './routes/Registration/Registration'
 import NewProjectForm from './Components/NewProjectForm/NewProjectForm';
 import Profile from './Components/Profile/Profile';
-<<<<<<< HEAD
-
-function App() {
-  return (
-    <div className="App">
-      <TopNav></TopNav>
-      <main>
-        <Switch>
-          <Route exact path={"/"} component={MainPage} />
-          <Route path={"/login"} component={Login} />
-          <Route path={"/register"} component={Registration} />
-          <Route path={"/new"} component={NewProjectForm} />
-          
-        </Switch>
-        <Route exact path={"/users/3"} component={Profile} />
-      </main>
-    </div>
-  );
-=======
 import MblNav from './Components/MblNav/MblNav';
 
 
@@ -57,6 +38,5 @@ export default class App extends React.Component {
       </div>
     );
   }    
->>>>>>> 829d63af4387b08b7c0abd76b3a5e1ad4d7aad26
 }
 
