f7d7d5f49393a24d8a1c956454d997e0beec9a88
quonn <qber83@gmail.com>
2019-06-06 09:48:16 -0700

    added AccountPanel and TipBox to MblNav

--- src/Components/MblNav/MblNav.js
+++ src/Components/MblNav/MblNav.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import TipBox from "../TipBox/TipBox";
+import AccountPanel from "../AccountPanel/AccountPanel";
 
 export default class MblNav extends React.Component {
     render() {
         const open = (!this.props.open && 'invisible') || '';
         return (
-            <aside id="mbl-nav" className={open}></aside>
+            <aside id="mbl-nav" className={open}>
+                <AccountPanel></AccountPanel>
+                <TipBox></TipBox>
+            </aside>
         )
     }
    
