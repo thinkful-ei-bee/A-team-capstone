5734738fa117f374dd6b94fa6c58b602ba00f024
David Bolin <david@bolin.at>
2019-06-06 12:01:24 -0700

    another url fix

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
     // http://localhost:8000/api
-    API_ENDPOINT : 'https://thinkful-final-capstone.herokuapp.com',
+    API_ENDPOINT : 'https://thinkful-final-capstone.herokuapp.com/api',
     TOKEN_KEY: 'capstone-auth-token',
   }
