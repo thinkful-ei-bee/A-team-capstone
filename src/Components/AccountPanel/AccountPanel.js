3043e545287296de9ba8891710550493df8716d8
Eddie Bueno <eddiebueno93@gmail.com>
2019-06-06 07:06:21 -0700

    added username and projects

--- src/Components/AccountPanel/AccountPanel.js
+++ src/Components/AccountPanel/AccountPanel.js
@@ -1,16 +1,45 @@
 import React from 'react';
+import ProfileApiService from '../../services/profile-api-service';
+import ProjectApiService from '../../services/project-api-service';
 
 export default class AccountPanel extends React.Component {
 
+  state = {
+    profile: {},
+    projects:[],
+    bids: {},
+  }
+
+  // get the profile to update the state when component mounts
+  componentDidMount(){
+    ProfileApiService.getProfile()
+      .then(profile=>{
+        console.log(profile)
+        this.setState({
+          profile,
+      }, 
+      ()=>{
+        ProjectApiService.getProjectsForUser(this.state.profile.id)
+          .then(projects=>{
+            this.setState({
+              projects,
+            })
+          })
+      })
+    })
+  }
+
     render() {
+      const projects = [];
+      this.state.projects.forEach(project=>projects.push(project.project_name))
         return (
             <article className="account-panel">
-                USERNAME
-                        <h4>Projects:</h4>
-                React Project....
-                        <h4>Bids:</h4>
+                {this.state.profile.username}
+                <h4>Projects:</h4>
+                {projects}
+                <h4>Bids:</h4>
                 3 Bids Pending....
-                    </article>
+            </article>
         )
     }
 }
