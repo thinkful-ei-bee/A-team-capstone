5b498299cad9f91e61ec05cd4898f8af9233b9a2
quonn <qber83@gmail.com>
2019-06-07 09:26:33 -0700

    fixed contrast issue and updated logo style

--- src/Components/TopNav/TopNav.js
+++ src/Components/TopNav/TopNav.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import HamburgerButton from "../HamburgerButton/HamburgerButton";
+import TokenService from '../../services/token-service';
 import "./TopNav.css";
 import NavMenu from "../NavMenu/NavMenu";
 
 class TopNav extends React.Component {
+
     render() {
         return (
             <nav className="top-nav">
                 <div className="nav-grid">
                     <div className="nav-grid-item">
                         <Link to={'/'} id="logo">
-                            <h1>BidHub</h1>
+                            <h1><span style={{color: "red"}}>Bid</span>Hub</h1>
                         </Link>
                     </div>
                     <div className="nav-grid-item-2">
