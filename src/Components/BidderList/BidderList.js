edd3292e0c578cee727ad5be377b992b34bb11d0
Eddie Bueno <eddiebueno93@gmail.com>
2019-06-11 12:20:14 -0700

    add props to Bidder component, remove ul

--- src/Components/BidderList/BidderList.js
+++ src/Components/BidderList/BidderList.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import Bidder from '../Bidder/Bidder';
-import TokenService from '../../services/token-service';
 
 class BidderList extends React.Component {
-
-  handleAcceptOnClick = () => {
-    // function to set user to ready to be changed to collaborator
-  }
-
-  handleDeclineOnClick = () => {
-    // function to set user to ready to be delined as collaborator
-  }
 
     render() {
       const bidders = [];
       this.props.bidders.forEach(bidder=>{
-        bidders.push(<Bidder username={bidder.username}/>)
+        bidders.push(
+        <Bidder 
+          onDeclineClick={this.props.onDeclineClick} 
+          onAcceptClick={this.props.onAcceptClick} 
+          username={bidder.username}
+        />)
       })
         return (
-          <ul>
+          <>
             {bidders}
-          </ul>
+          </>
         );
     }
 }
