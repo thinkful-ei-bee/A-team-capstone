90b30754c68df240f630fad630d388153d9ec8a0
Eddie Bueno <eddiebueno93@gmail.com>
2019-06-11 16:03:40 -0700

    add userId sent as props

--- src/Components/BidderList/BidderList.js
+++ src/Components/BidderList/BidderList.js
@@ -6,8 +6,10 @@ class BidderList extends React.Component {
     render() {
       const bidders = [];
       this.props.bidders.forEach(bidder=>{
+        console.log(bidder);
         bidders.push(
         <Bidder 
+          userId={bidder.user_id}
           onDeclineClick={this.props.onDeclineClick} 
           onAcceptClick={this.props.onAcceptClick} 
           username={bidder.username}
