80a44d2c8067df045a5fbbd59b2d7751ffda5288
Eddie Bueno <eddiebueno93@gmail.com>
2019-06-11 08:47:42 -0700

    add get bidders to bids api service

--- src/services/bids-api-service.js
+++ src/services/bids-api-service.js
@@ -30,6 +30,20 @@ const BidsApiService = {
           ? res.json().then(e=>Promise.reject(e))
           : res.json()
       );
+  },
+  getBidders(project_id){
+    return fetch(`${config.API_ENDPOINT}/bids/others/${project_id}`,{
+      method: 'GET',
+      headers:{
+        'Content-Type':'application/json',
+        'Authorization': `bearer ${TokenService.getAuthToken()}`
+      }
+    })
+      .then(res=>
+        (!res.ok)
+          ? res.json().then(e=>Promise.reject(e))
+          : res.json()
+      );
   }
 }
 
