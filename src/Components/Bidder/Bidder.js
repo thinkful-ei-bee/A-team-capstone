ab52546b9fdccefff1b058cee2f1179b2b4eb870
Eddie Bueno <eddiebueno93@gmail.com>
2019-06-11 16:02:54 -0700

    change value to use userId instead of id

--- src/Components/Bidder/Bidder.js
+++ src/Components/Bidder/Bidder.js
@@ -6,12 +6,12 @@ class Bidder extends React.Component {
     render() {
 
         return (
-          <div key={this.props.username}>
+          <div key={this.props.userId}>
             <h3><i>{this.props.username}</i></h3>
             <label htmlFor={`${this.props.username}-accept`}>Accept</label> 
-            <input onClick={this.props.onAcceptClick} id={`${this.props.username}-accept`} type="radio" name={this.props.username} className='btn green-text' value={this.props.username}/>
+            <input onClick={this.props.onAcceptClick} id={`${this.props.userId}-accept`} type="radio" name={this.props.username} className='btn green-text' value={this.props.userId}/>
             <label htmlFor={`${this.props.username}-decline`}>Decline</label>
-            <input onClick={this.props.onDeclineClick} id={`${this.props.username}-decline`} type="radio" name={this.props.username}className='btn red-text' value={this.props.username}/>
+            <input onClick={this.props.onDeclineClick} id={`${this.props.userId}-decline`} type="radio" name={this.props.username}className='btn red-text' value={this.props.userId}/>
           </div>
         );
     }
