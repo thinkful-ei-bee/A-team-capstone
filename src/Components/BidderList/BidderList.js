b42e37495c8201375c6072e92d9c0910cad5cef1
Eddie Bueno <eddiebueno93@gmail.com>
2019-06-11 09:21:30 -0700

    display username on bidder list

--- src/Components/BidderList/BidderList.js
+++ src/Components/BidderList/BidderList.js
@@ -3,7 +3,6 @@ import Bidder from '../Bidder/Bidder';
 import TokenService from '../../services/token-service';
 
 class BidderList extends React.Component {
-  // will need props of list of bidders
 
   handleAcceptOnClick = () => {
     // function to set user to ready to be changed to collaborator
@@ -16,7 +15,7 @@ class BidderList extends React.Component {
     render() {
       const bidders = [];
       this.props.bidders.forEach(bidder=>{
-        bidders.push(<Bidder username={bidder.id}/>)
+        bidders.push(<Bidder username={bidder.username}/>)
       })
         return (
           <ul>
