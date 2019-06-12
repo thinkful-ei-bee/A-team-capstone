f515c3e905a60db68b06618303b6a363a32f82f0
Eddie Bueno <eddiebueno93@gmail.com>
2019-06-12 08:25:38 -0700

    change values for props

--- src/Components/Bidder/Bidder.js
+++ src/Components/Bidder/Bidder.js
@@ -4,14 +4,13 @@ class Bidder extends React.Component {
 
 
     render() {
-
         return (
-          <div key={this.props.userId}>
-            <h3><i>{this.props.username}</i></h3>
-            <label htmlFor={`${this.props.username}-accept`}>Accept</label> 
-            <input onClick={this.props.onAcceptClick} id={`${this.props.userId}-accept`} type="radio" name={this.props.username} className='btn green-text' value={this.props.userId}/>
-            <label htmlFor={`${this.props.username}-decline`}>Decline</label>
-            <input onClick={this.props.onDeclineClick} id={`${this.props.userId}-decline`} type="radio" name={this.props.username}className='btn red-text' value={this.props.userId}/>
+          <div key={this.props.bidder.user_id}>
+            <h3><i>{this.props.bidder.username}</i></h3>
+            <label htmlFor={`${this.props.bidder.username}-accept`}>Accept</label> 
+            <input onClick={this.props.onAcceptClick} id={`${this.props.bidder.user_id}-accept`} type="radio" name={this.props.bidder.username} className='btn green-text' value={this.props.bidder.user_id}/>
+            <label htmlFor={`${this.props.bidder.username}-decline`}>Decline</label>
+            <input onClick={this.props.onDeclineClick} id={`${this.props.bidder.user_id}-decline`} type="radio" name={this.props.bidder.username}className='btn red-text' value={this.props.bidder.user_id}/>
           </div>
         );
     }
