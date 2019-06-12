2bb05f6568901338830fe2e77ea016232affde0d
Eddie Bueno <eddiebueno93@gmail.com>
2019-06-12 09:02:42 -0700

    merge conflict add update Bid

--- src/services/bids-api-service.js
+++ src/services/bids-api-service.js
@@ -3,7 +3,6 @@ import TokenService from './token-service';
 
 const BidsApiService = {
   // /bids/others/:bid_id for PATCH
-<<<<<<< HEAD
   updateBid(bid){
     return fetch(`${config.API_ENDPOINT}/bids/others/${bid.id}`,{
       method: 'PATCH',
@@ -14,8 +13,6 @@ const BidsApiService = {
       body: JSON.stringify(bid)
     })
   },
-=======
->>>>>>> 601214ab03d94626feb9699946a8fec4e81c6b1d
   postBid(bid) {
     return fetch(`${config.API_ENDPOINT}/bids`,{
       method: 'POST',
