eecb68f8d3d73321ba482316b0c6d5bc96ee4724
David Bolin <david@bolin.at>
2019-06-13 08:26:48 -0700

    post and get for comments

---
+++ src/services/comments-api-service.js
@@ -0,0 +1,37 @@
+import config from '../config';
+import TokenService from './token-service';
+
+const commentsApiService = {
+  getComments(project_id) {
+    return fetch(`${config.API_ENDPOINT}/comments/${project_id}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `bearer ${TokenService.getAuthToken()}`
+      }
+    })
+    .then(res =>
+      (!res.ok)
+        ? res.json().then(e => Promise.reject(e))
+        : res.json()
+    );
+  },
+
+  postComment(project_id, comment) {
+    return fetch(`${config.API_ENDPOINT}/comments/${project_id}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `bearer ${TokenService.getAuthToken()}`
+      },
+      body: JSON.stringify(comment),
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      );
+  }
+}
+
+export default commentsApiService;
