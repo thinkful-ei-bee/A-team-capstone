9ee9b7734442665b0e0fa7289c20321ff351ff2f
David Bolin <david@bolin.at>
2019-06-18 08:24:33 -0700

    testing a change

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
     // http://localhost:8000/api
     API_ENDPOINT : 'https://thinkful-final-capstone.herokuapp.com/api',
-    WS_ENDPOINT : 'wss://bidhub.now.sh',
+    WS_ENDPOINT : 'wss://bidhub.now.sh/websocket',
     TOKEN_KEY: 'capstone-auth-token',
   }
