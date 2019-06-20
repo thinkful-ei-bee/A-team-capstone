a72c4462800601790520232c04e5777f84f379c5
Eddie Bueno <eddiebueno93@gmail.com>
2019-06-20 09:13:50 -0700

    add error message

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -48,6 +48,7 @@ export default class LandingPage extends React.Component {
     }
 
     render() {
+        let { error } = this.state;
         const mitten = <FontAwesomeIcon icon={faMitten} className="thumbsUp" style={{ fontSize: "30px" }} />
         return (
             <div>
