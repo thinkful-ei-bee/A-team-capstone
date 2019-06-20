151c14e0f94f9979e4807a937323d436cfb0867a
Eddie Bueno <eddiebueno93@gmail.com>
2019-06-20 13:47:23 -0700

    add updateAccount state for account panel

--- src/Components/MblNav/MblNav.js
+++ src/Components/MblNav/MblNav.js
@@ -4,13 +4,24 @@ import AccountPanel from "../AccountPanel/AccountPanel";
 import NavMenu from "../NavMenu/NavMenu";
 
 export default class MblNav extends React.Component {
+
+  state={
+    updateAccount:true,
+  }
+
+  setUpdateAccount = () =>{
+    this.setState({
+      updateAccount: !this.state.updateAccount,
+    })
+  }
+  
     render() {
         const open = (!this.props.open && 'invisible') || '';
         return (
             <aside id="mbl-nav" className={open}>
                 {/* <Logo id={-mblnav}></Logo> */}
                 <NavMenu swapOpen={this.props.swapOpen}></NavMenu>
-                <AccountPanel></AccountPanel>
+                <AccountPanel open={this.props.open} setUpdateAccount={this.setUpdateAccount} updateAccount={this.state.updateAccount}></AccountPanel>
                 <TipBox></TipBox>
             </aside>
         )
